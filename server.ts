f0b71617b481241a704b571d2d9f1cc25e0b6c48
mohamed-el-sharkawy <melsharkawy346@gmail.com>
2022-05-30 17:22:01 +0200

    emit messages

--- server.ts
+++ server.ts
@@ -11,6 +11,18 @@ const io = new Server(server);
 
 io.on('connection', (socket) => {
     console.log('New WS connection');
+
+    socket.emit('message', 'Welcome to the ChatApp');
+
+    socket.broadcast.emit('message', 'New user joined');
+
+    socket.on('disconnect', () => {
+        io.emit('message', 'A user has left the chat');
+    });
+
+    socket.on('chatMessage', (msg) => {
+        io.emit('chatMessage', msg);
+    });
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
