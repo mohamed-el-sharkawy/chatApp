70a0cc95cc73a1c5dbbf8950127d667d4753a4cb
mohamed-el-sharkawy <melsharkawy346@gmail.com>
2022-05-30 17:50:34 +0200

    output message

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,36 @@
+const chatForm = document.getElementById('chat-form');
+const chatMessages = document.querySelector('.chat-messages');
+
 const socket = io();
+
+socket.on('message', (message) => {
+    outMessage(message);
+});
+
+socket.on('chatMessage', (message) => {
+    console.log(message);
+
+    outMessage(message);
+    chatMessages.scrollTop = chatMessages.scrollHeight;
+});
+
+chatForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const msg = e.target.elements.msg.value;
+
+    socket.emit('chatMessage', msg);
+
+    e.target.elements.msg.value = '';
+    e.target.elements.msg.focus();
+});
+
+function outMessage(msg) {
+    const div = document.createElement('div');
+    div.classList.add('message');
+    div.innerHTML = `<p class="meta">Brad <span>9:12pm</span></p>
+  <p class="text">
+    ${msg}
+  </p>`;
+    document.querySelector('.chat-messages').appendChild(div);
+}
