f1b86729190966d43ee8da577ca06614e5fd3ea1
mohamed-el-sharkawy <melsharkawy346@gmail.com>
2022-06-03 22:03:46 +0200

    connect to mongoDB and create user schema

--- server.ts
+++ server.ts
@@ -1,8 +1,11 @@
 import path from 'path';
 import express from 'express';
 import http from 'http';
-import socketio from 'socket.io';
+import mongoose from 'mongoose';
+import 'dotenv/config';
 import { Server } from 'socket.io';
+import { formatMessages } from './utils/formatMessages';
+import { User } from './models/User';
 
 const app = express();
 const server = http.createServer(app);
@@ -10,18 +13,30 @@ const server = http.createServer(app);
 const io = new Server(server);
 
 io.on('connection', (socket) => {
-    console.log('New WS connection');
+    socket.on('joinRoom', ({ username, room }) => {
+        socket.emit(
+            'message',
+            formatMessages('ChatApp', `Welcome to ${room} room`)
+        );
 
-    socket.emit('message', 'Welcome to the ChatApp');
-
-    socket.broadcast.emit('message', 'New user joined');
-
-    socket.on('disconnect', () => {
-        io.emit('message', 'A user has left the chat');
+        socket.broadcast.emit(
+            'message',
+            formatMessages(
+                'ChatApp',
+                `${username} has joinded the room ${room}`
+            )
+        );
     });
 
     socket.on('chatMessage', (msg) => {
-        io.emit('chatMessage', msg);
+        io.emit('chatMessage', formatMessages('USER', msg));
+    });
+
+    socket.on('disconnect', () => {
+        io.emit(
+            'message',
+            formatMessages('ChatApp', 'A user has left the chat')
+        );
     });
 });
 
@@ -29,6 +44,17 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 const PORT = process.env.PORT || 3000;
 
-server.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
-});
+start();
+
+async function start() {
+    try {
+        mongoose.connect(process.env.MONGO_URI!, () => {
+            console.log('Connected to MongoDB');
+        });
+    } catch (error) {
+        console.error(error);
+    }
+    server.listen(PORT, () => {
+        console.log(`Server started on port ${PORT}`);
+    });
+}
